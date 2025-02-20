686927a13356aa7f78f5992c3d3ed3f50700b116
eiramevi <angelajlamera@gmail.com>
2025-02-21 00:28:19 +0800

    added design

--- src/server.js
+++ src/server.js
@@ -1,37 +1,25 @@
-const express = require("express")
-
+const express = require("express");
 const path = require("path");
-
-const dotenv = require("dotenv")
-
+const dotenv = require("dotenv");
 const pokemonRouter = require("../routes/pokemon");
-
-
 
 dotenv.config();
 
-
-
 const app = express();
 
-
-
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.use(express.static(path.join(__dirname, "public")));
-
-
+// app.get("/", (req, res) => {
+//   return res.status(200).json({
+//     message: "hello world",
+//   });
+// });
 
 app.use("/", pokemonRouter);
 
+const PORT = 5000 || process.env.PORT;
 
-
-const PORT = 8000 || process.env.PORT;
-
-
-
-app.listen(PORT,()=>{
-
-  console.log(`Server is running on port http://localhost:${PORT}`)
-
-})
+app.listen(PORT, () => {
+  console.log(`Server is Running on http://localhost:${PORT}`);
+});
